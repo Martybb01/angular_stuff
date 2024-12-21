93bc0675a8b13ef9c81096ac6f9fddf226642c32
Martina Boccuzzi <martybb01@gmail.com>
2024-12-21 12:31:14 +0100

    add user-input component form + 2way binding props

--- 02-investment-project/src/app/user-input/user-input.component.ts
+++ 02-investment-project/src/app/user-input/user-input.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  investmentReturn = '999';
+  years = '5';
 
+  onSubmit() {
+    console.log('User input submitted');
+    console.log('Initial investment: ' + this.initialInvestment);
+    console.log('Annual investment: ' + this.annualInvestment);
+    console.log('Investment return: ' + this.investmentReturn);
+    console.log('Years: ' + this.years);
+  }
 }
